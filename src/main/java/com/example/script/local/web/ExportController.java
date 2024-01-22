246baec7f09638d0f8591d45fbb9484dd93e26f4
albert lewis <1466329700@qq.com>
2024-01-22 10:23:35 +0800

    fix:修复cn.hutool.core.lang.Snowflake反射问题

--- src/main/java/com/example/script/local/web/ExportController.java
+++ src/main/java/com/example/script/local/web/ExportController.java
@@ -2,6 +2,7 @@ package com.example.script.local.web;
 
 import com.alibaba.druid.util.JdbcConstants;
 import com.example.script.common.domain.DatabaseInfo;
+import com.example.script.common.entity.*;
 import com.example.script.common.factory.DataSourceFactory;
 import com.example.script.common.strategy.DataSourceStrategy;
 import com.example.script.local.service.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static com.example.script.test.constant.DBConstant.*;
 
@@ -35,18 +37,18 @@ public class ExportController {
     @GetMapping
     public DatabaseInfo saveData() throws SQLException {
         DatabaseInfo dbData = getDbData();
-//        List<SyncDb> dbList = dbData.getDbList();
-//        List<SyncTable> tableList= dbData.getTableList();
-//        List<SyncCol> colList=dbData.getColList();
-//        List<SyncIndex> indexList=dbData.getIndexList();
-//        List<SyncFk> fkList=dbData.getFkList();
-//        List<SyncData> dataList = dbData.getDataList();
-//        syncDbService.saveBatch(dbList);
-//        syncTableService.saveBatch(tableList);
-//        syncColService.saveBatch(colList);
-//        syncIndexService.saveBatch(indexList);
-//        syncFkService.saveBatch(fkList);
-//        syncDataService.saveBatch(dataList);
+        List<SyncDb> dbList = dbData.getDbList();
+        List<SyncTable> tableList= dbData.getTableList();
+        List<SyncCol> colList=dbData.getColList();
+        List<SyncIndex> indexList=dbData.getIndexList();
+        List<SyncFk> fkList=dbData.getFkList();
+        List<SyncData> dataList = dbData.getDataList();
+        syncDbService.saveBatch(dbList);
+        syncTableService.saveBatch(tableList);
+        syncColService.saveBatch(colList);
+        syncIndexService.saveBatch(indexList);
+        syncFkService.saveBatch(fkList);
+        syncDataService.saveBatch(dataList);
         return dbData;
     }
     private DatabaseInfo getDbData() throws SQLException {
